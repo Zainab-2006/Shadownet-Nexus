6c355714e30a3e19210a8ef321f6154b85662de1
Zainab-2006 <zbharmal2006@gmail.com>
2026-04-18 09:09:43 +0530

    Remove checked-in local secret defaults

--- springboot/run-mysql.bat
+++ springboot/run-mysql.bat
@@ -6,6 +6,10 @@ echo DB URL: jdbc:mysql://127.0.0.1:3305/shadownet?createDatabaseIfNotExist=true
 echo Server: http://localhost:3001
 echo.
 
-java -Dspring.profiles.active=mysql -Dspring.datasource.url="jdbc:mysql://127.0.0.1:3305/shadownet?createDatabaseIfNotExist=true&useSSL=false&allowPublicKeyRetrieval=true" -Dspring.datasource.username=root -Dspring.datasource.password=root -Djwt.secret=test -Dflyway.enabled=true -jar target/shadownet-nexus-1.0.0.jar
+if "%DB_USERNAME%"=="" set DB_USERNAME=shadownet
+if "%DB_PASSWORD%"=="" set DB_PASSWORD=change-me-local-only
+if "%JWT_SECRET%"=="" set JWT_SECRET=change-me-local-only-jwt-secret-64-characters-minimum
+
+java -Dspring.profiles.active=mysql -Dspring.datasource.url="jdbc:mysql://127.0.0.1:3305/shadownet?createDatabaseIfNotExist=true&useSSL=false&allowPublicKeyRetrieval=true" -Dspring.datasource.username="%DB_USERNAME%" -Dspring.datasource.password="%DB_PASSWORD%" -Djwt.secret="%JWT_SECRET%" -Dflyway.enabled=true -jar target/shadownet-nexus-1.0.0.jar
 
 pause
